6c9d5ab9141e6557d57db3ad4a04aa74dc7d68be
Adam Hunt <avh17@uclive.ac.nz>
2016-03-23 19:08:20 +1300

    Basic Gui Scene

--- src/main/App.java
+++ src/main/App.java
@@ -1,10 +1,32 @@
 package main;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import sun.applet.Main;
+
 /**
  * Created by Adam on 23/03/2016.
  */
-public class App {
+public class App extends Application{
+
     public static void main(String[] args) {
-        System.out.println("Test");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        AnchorPane pane = FXMLLoader.load(Main.class.getResource("/resources/MainScene.fxml"));
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("EventPlanner");
+        primaryStage.setHeight(600);
+        primaryStage.setWidth(400);
+        primaryStage.setMinHeight(600);
+        primaryStage.setMinWidth(400);
+        primaryStage.show();
+
     }
 }
