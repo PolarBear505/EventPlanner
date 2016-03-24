ca84f4822e70e01fedeff31c0fabc6c6b0df8e20
Adam Hunt <avh17@uclive.ac.nz>
2016-03-24 21:27:15 +1300

    Allowed an fxml to be injected into the main scroll pane

--- src/main/controllers/MainSceneController.java
+++ src/main/controllers/MainSceneController.java
@@ -2,18 +2,30 @@ package main.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /**
  * Created by Adam on 24/03/2016.
  */
 public class MainSceneController extends ParentController {
 
+    @FXML
+    ScrollPane scrollPane;
+
+    @FXML
+     VBox contentBox = new VBox();
+
     public MainSceneController() {
         mainScene = this;
     }
 
     @FXML
-    public void newEventPressed(ActionEvent event) {
-        System.out.println("It Worked");
+    public void newEventPressed(ActionEvent event) throws Exception{
+        Pane newPane = FXMLLoader.load(getClass().getResource("/resources/Event.fxml"));
+        contentBox.getChildren().addAll(newPane);
+        scrollPane.setContent(contentBox);
     }
 }
